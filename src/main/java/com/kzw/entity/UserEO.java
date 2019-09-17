295306ec350d94070dcc3ec42567adc53d9e7b33
kongzhiwei-007 <919648199@qq.com>
2019-09-17 15:41:38 +0800

    gg

--- src/main/java/com/kzw/entity/UserEO.java
+++ src/main/java/com/kzw/entity/UserEO.java
@@ -22,9 +22,9 @@ public class UserEO {
     private Long id;
 
     private String userName;
-    private String userCode;
-    private String vipFlag;
-    private String uniqueFlag;
+    private String userCode;//openId
+    private String vipFlag;//是否VIP 1是0否
+    private String uniqueFlag;//微信用户唯一标志(本系统内的)
 
     public String getUniqueFlag() {
         return uniqueFlag;
