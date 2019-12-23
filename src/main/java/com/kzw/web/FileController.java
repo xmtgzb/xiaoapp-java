227ccefec091d17f787c175ad728ea6f0c20467d
Carlos <qihai323@foxmail.com>
2019-12-23 16:22:52 +0800

    dd

--- src/main/java/com/kzw/web/FileController.java
+++ src/main/java/com/kzw/web/FileController.java
@@ -31,10 +31,7 @@ import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 
@@ -279,135 +276,45 @@ public class FileController {
 
     @RequestMapping(value = "/uploadOne", method = { RequestMethod.POST,RequestMethod.GET})
     @ResponseBody
-    public Result uploadImage(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    public Result uploadImage(HttpServletRequest request, @RequestParam("file")MultipartFile[] files) throws IOException {
 
 //        MultipartHttpServletRequest req =(MultipartHttpServletRequest)request;
 //        MultipartFile multipartFile =  req.getFile("file");
-//        try {
-//
-//            String uniqueFlag =(String)request.getHeader("uniqueFlag");
-//            UserEO user =  SystemConstant.USER_MAP.get(uniqueFlag);
-//            if(user==null){
-//                return Result.error("请先登录！");
-//            }
-//            String realPath = SystemConstant.PHOTO_PATH+user.getUserCode()+"/";
-//            String sltPath = realPath+SystemConstant.PHOTO_THUM_PATH;
-//            File dir = new File(realPath);
-//            File dirThum = new File(sltPath);
-//            if (!dir.exists()) {
-//                dir.mkdirs();
-//            }
-//            if (!dirThum.exists()) {
-//                dirThum.mkdirs();
-//            }
-//            String fileName =new Date().getTime()+".jpg";
-//            String fileDesc =(String)request.getParameter("fileDesc");
-//            String isSee =(String)request.getParameter("isSee");
-//            if (StringUtils.isEmpty(isSee)){
-//                isSee="0";
-//            }
-//            String xiangCe =(String)request.getParameter("xiangCe");
-//            File file  =  new File(realPath,fileName);
-////            File fileThum  =  new File(sltPath,fileName);
-//            Result res=checkSize(multipartFile,user.getVipFlag(),realPath);
-//            if(res.isError()){
-//                return res;
-//            }
-//            multipartFile.transferTo(file);//真实图片
-//            //生成缩略图片
-//            ImageUtil.generateThumbnail2Directory(sltPath,realPath+fileName);
-//            UserFileEO userFile=new UserFileEO();
-//            userFile.setFileDesc(fileDesc);//文件描述
-//            userFile.setFileName(fileName);//文件名
-//            userFile.setSltName(fileName.substring(0,fileName.lastIndexOf("."))+"-thumbnail"+fileName.substring(fileName.lastIndexOf(".")));
-//            userFile.setInsertTime(new Date());
-//            userFile.setIsSee(isSee);//是否对外可见 1是，0否
-//            userFile.setXiangCe(xiangCe);//相册
-//            userFile.setRealPath(realPath);
-//            userFile.setSltPath(sltPath);
-//            userFile.setUserCode(user.getUserCode());
-//            userFileService.save(userFile);
-//        } catch (Exception e) {
-//            log.error("图片上传失败",e);
-//            return  Result.error("图片上传失败");
-//        }
-        String uniqueFlag =(String)request.getHeader("uniqueFlag");
-        UserEO user =  SystemConstant.USER_MAP.get(uniqueFlag);
-        if(user==null){
-            return Result.error("请先登录！");
-        }
-        //获取文件需要上传到的路径
-        String realPath = SystemConstant.PHOTO_PATH+user.getUserCode()+"/";
-        String sltPath = realPath+SystemConstant.PHOTO_THUM_PATH;
-
-        String fileDesc =(String)request.getParameter("fileDesc");
-        String isSee =(String)request.getParameter("isSee");
-        if (StringUtils.isEmpty(isSee)){
-            isSee="0";
-        }
-        File dir = new File(realPath);
-        File dirThum = new File(sltPath);
-        if (!dir.exists()) {
-            dir.mkdirs();
-        }
-        if (!dirThum.exists()) {
-            dirThum.mkdirs();
-        }
-        String xiangCe =(String)request.getParameter("xiangCe");
-        request.setCharacterEncoding("utf-8");  //设置编码
-        //获得磁盘文件条目工厂
-        DiskFileItemFactory factory = new DiskFileItemFactory();
-
-        //如果没以下两行设置的话,上传大的文件会占用很多内存，
-        //设置暂时存放的存储室,这个存储室可以和最终存储文件的目录不同
-        /**
-         * 原理: 它是先存到暂时存储室，然后再真正写到对应目录的硬盘上，
-         * 按理来说当上传一个文件时，其实是上传了两份，第一个是以 .tem 格式的
-         * 然后再将其真正写到对应目录的硬盘上
-         */
-        factory.setRepository(dir);
-        //设置缓存的大小，当上传文件的容量超过该缓存时，直接放到暂时存储室
-        factory.setSizeThreshold(1024 * 1024);
-        //高水平的API文件上传处理
-        ServletFileUpload upload = new ServletFileUpload(factory);
         try {
-            List<FileItem> list = upload.parseRequest(request);
-            FileItem picture = null;
-            for (FileItem item : list) {
-                //获取表单的属性名字
-                String name = item.getFieldName();
-                //如果获取的表单信息是普通的 文本 信息
-                if (item.isFormField()) {
-                    //获取用户具体输入的字符串
-                    String value = item.getString();
-                    request.setAttribute(name, value);
-                    log.info("name=" + name + ",value=" + value);
-                } else {
-                    picture = item;
-                }
-            }
+            for(MultipartFile multipartFile:files){
 
-            //自定义上传图片的名字为userId.jpg
-//            String fileName = request.getAttribute("userId") + ".jpg";
+
+            String uniqueFlag =(String)request.getHeader("uniqueFlag");
+            UserEO user =  SystemConstant.USER_MAP.get(uniqueFlag);
+            if(user==null){
+                return Result.error("请先登录！");
+            }
+            String realPath = SystemConstant.PHOTO_PATH+user.getUserCode()+"/";
+            String sltPath = realPath+SystemConstant.PHOTO_THUM_PATH;
+            File dir = new File(realPath);
+            File dirThum = new File(sltPath);
+            if (!dir.exists()) {
+                dir.mkdirs();
+            }
+            if (!dirThum.exists()) {
+                dirThum.mkdirs();
+            }
             String fileName =new Date().getTime()+".jpg";
-            File fileThum  =  new File(sltPath,fileName);
-            ImageUtil.generateThumbnail2Directory(sltPath,realPath+fileName);
-            String destPath = sltPath + fileName;
-            log.info("destPath=" + destPath);
-
-            //真正写到磁盘上
-            File file = new File(destPath);
-            OutputStream out = new FileOutputStream(file);
-            InputStream in = picture.getInputStream();
-            int length = 0;
-            byte[] buf = new byte[1024];
-            // in.read(buf) 每次读到的数据存放在buf 数组中
-            while ((length = in.read(buf)) != -1) {
-                //在buf数组中取出数据写到（输出流）磁盘上
-                out.write(buf, 0, length);
+            String fileDesc =(String)request.getParameter("fileDesc");
+            String isSee =(String)request.getParameter("isSee");
+            if (StringUtils.isEmpty(isSee)){
+                isSee="0";
             }
-            in.close();
-            out.close();
+            String xiangCe =(String)request.getParameter("xiangCe");
+            File file  =  new File(realPath,fileName);
+//            File fileThum  =  new File(sltPath,fileName);
+            Result res=checkSize(multipartFile,user.getVipFlag(),realPath);
+            if(res.isError()){
+                return res;
+            }
+            multipartFile.transferTo(file);//真实图片
+            //生成缩略图片
+            ImageUtil.generateThumbnail2Directory(sltPath,realPath+fileName);
             UserFileEO userFile=new UserFileEO();
             userFile.setFileDesc(fileDesc);//文件描述
             userFile.setFileName(fileName);//文件名
@@ -419,20 +326,114 @@ public class FileController {
             userFile.setSltPath(sltPath);
             userFile.setUserCode(user.getUserCode());
             userFileService.save(userFile);
-        } catch (FileUploadException e1) {
-            log.error("", e1);
+            }
         } catch (Exception e) {
-            log.error("", e);
+            log.error("图片上传失败",e);
+            return  Result.error("图片上传失败");
         }
-
-
-        PrintWriter printWriter = response.getWriter();
-        response.setContentType("application/json");
-        response.setCharacterEncoding("utf-8");
-        HashMap<String, Object> res = new HashMap<String, Object>();
-        res.put("success", true);
-        printWriter.write(JSON.toJSONString(res));
-        printWriter.flush();
+//        String uniqueFlag =(String)request.getHeader("uniqueFlag");
+//        UserEO user =  SystemConstant.USER_MAP.get(uniqueFlag);
+//        if(user==null){
+//            return Result.error("请先登录！");
+//        }
+//        //获取文件需要上传到的路径
+//        String realPath = SystemConstant.PHOTO_PATH+user.getUserCode()+"/";
+//        String sltPath = realPath+SystemConstant.PHOTO_THUM_PATH;
+//
+//        String fileDesc =(String)request.getParameter("fileDesc");
+//        String isSee =(String)request.getParameter("isSee");
+//        if (StringUtils.isEmpty(isSee)){
+//            isSee="0";
+//        }
+//        File dir = new File(realPath);
+//        File dirThum = new File(sltPath);
+//        if (!dir.exists()) {
+//            dir.mkdirs();
+//        }
+//        if (!dirThum.exists()) {
+//            dirThum.mkdirs();
+//        }
+//        String xiangCe =(String)request.getParameter("xiangCe");
+//        request.setCharacterEncoding("utf-8");  //设置编码
+//        //获得磁盘文件条目工厂
+//        DiskFileItemFactory factory = new DiskFileItemFactory();
+//
+//        //如果没以下两行设置的话,上传大的文件会占用很多内存，
+//        //设置暂时存放的存储室,这个存储室可以和最终存储文件的目录不同
+//        /**
+//         * 原理: 它是先存到暂时存储室，然后再真正写到对应目录的硬盘上，
+//         * 按理来说当上传一个文件时，其实是上传了两份，第一个是以 .tem 格式的
+//         * 然后再将其真正写到对应目录的硬盘上
+//         */
+//        factory.setRepository(dir);
+//        //设置缓存的大小，当上传文件的容量超过该缓存时，直接放到暂时存储室
+//        factory.setSizeThreshold(1024 * 1024);
+//        //高水平的API文件上传处理
+//        ServletFileUpload upload = new ServletFileUpload(factory);
+//        try {
+//            List<FileItem> list = upload.parseRequest(request);
+//            FileItem picture = null;
+//            for (FileItem item : list) {
+//                //获取表单的属性名字
+//                String name = item.getFieldName();
+//                //如果获取的表单信息是普通的 文本 信息
+//                if (item.isFormField()) {
+//                    //获取用户具体输入的字符串
+//                    String value = item.getString();
+//                    request.setAttribute(name, value);
+//                    log.info("name=" + name + ",value=" + value);
+//                } else {
+//                    picture = item;
+//                }
+//            }
+//
+//            //自定义上传图片的名字为userId.jpg
+////            String fileName = request.getAttribute("userId") + ".jpg";
+//            String fileName =new Date().getTime()+".jpg";
+//            File fileThum  =  new File(sltPath,fileName);
+//
+//            //真正写到磁盘上
+//            String destPath = sltPath + fileName;
+//            File file = new File(destPath);
+//            OutputStream out = new FileOutputStream(file);
+//            InputStream in = picture.getInputStream();
+//            int length = 0;
+//            byte[] buf = new byte[1024];
+//            // in.read(buf) 每次读到的数据存放在buf 数组中
+//            while ((length = in.read(buf)) != -1) {
+//                //在buf数组中取出数据写到（输出流）磁盘上
+//                out.write(buf, 0, length);
+//            }
+//            in.close();
+//            out.close();
+//            ImageUtil.generateThumbnail2Directory(sltPath,realPath+fileName);
+//
+//            log.info("destPath=" + destPath);
+//            UserFileEO userFile=new UserFileEO();
+//            userFile.setFileDesc(fileDesc);//文件描述
+//            userFile.setFileName(fileName);//文件名
+//            userFile.setSltName(fileName.substring(0,fileName.lastIndexOf("."))+"-thumbnail"+fileName.substring(fileName.lastIndexOf(".")));
+//            userFile.setInsertTime(new Date());
+//            userFile.setIsSee(isSee);//是否对外可见 1是，0否
+//            userFile.setXiangCe(xiangCe);//相册
+//            userFile.setRealPath(realPath);
+//            userFile.setSltPath(sltPath);
+//            userFile.setUserCode(user.getUserCode());
+//            userFileService.save(userFile);
+//        } catch (FileUploadException e1) {
+//            log.error("", e1);
+//        } catch (Exception e) {
+//            log.error("", e);
+//        }
+//
+//
+//        PrintWriter printWriter = response.getWriter();
+//        response.setContentType("application/json");
+//        response.setCharacterEncoding("utf-8");
+//        HashMap<String, Object> res = new HashMap<String, Object>();
+//        res.put("success", true);
+//        printWriter.write(JSON.toJSONString(res));
+//        printWriter.flush();
         return Result.ok("上传成功");
     }
     private Result checkSize(MultipartFile multipartFile,String vipFlag,String realPath){
